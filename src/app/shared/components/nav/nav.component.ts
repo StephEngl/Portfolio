7f7a10d3304f00fbf8e7b6951f1b6ff1f564dfcc
Stephanie Englberger <stephaniekoebich@aol.com>
2025-03-28 18:09:00 +0100

    Implemented translation function in nav-component

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AppComponent } from '../../../app.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [AppComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
   isMenuOpen = false;
+  currentLang: string = this.translate.getDefaultLang();;
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.currentLang = this.translate.currentLang;
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+    this.currentLang = lang;
   }
 }
