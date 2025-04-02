4a42daedecafcd2adf5aab5c1c0128e5e212d392
Stephanie Englberger <stephaniekoebich@aol.com>
2025-04-02 23:13:55 +0200

    Added typing effect to about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 import { NgxTypedJsModule } from 'ngx-typed-js';
@@ -5,10 +6,24 @@ import { NgxTypedJsModule } from 'ngx-typed-js';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [TranslatePipe, TranslateDirective, NgxTypedJsModule],
+  imports: [TranslatePipe, TranslateDirective, NgxTypedJsModule, CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-name = "Teamkollegen"
+  currentImage: string = 'url("../../assets/icons/icon_location.svg")';
+  fadeClass: string = 'fade-in';
+
+  toggleIcon(index: number): void {
+    this.fadeClass = 'fade-out';
+    setTimeout(() => {
+      if (index === 0) {
+        this.currentImage = 'url("../../assets/icons/icon_location.svg")';
+      } else if (index === 1) {
+        this.currentImage = 'url("../../assets/icons/icon_remote.svg")';
+      }
+
+      this.fadeClass = 'fade-in';
+    }, 200); // Dauer muss mit der Transition-Zeit übereinstimmen
+  }
 }
