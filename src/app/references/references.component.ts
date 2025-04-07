c5d2e0d670da1aad048a0b4e9f7a66ed4686ce56
Stephanie Englberger <stephaniekoebich@aol.com>
2025-04-07 17:08:01 +0200

    Added media query at references section for preventing horizontal scrolling above 965px screen width

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { Reference } from '../interfaces/reference';
 
