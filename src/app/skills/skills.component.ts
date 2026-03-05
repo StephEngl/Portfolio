879e7dc466fa762ec5a39e0fdfca11aee9a18903
Stephanie Englberger <stephaniekoebich@aol.com>
2026-03-05 13:34:27 +0100

    Enhance skillsets

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -26,23 +26,25 @@ export class SkillsComponent {
     { src: '../../assets/icons/skill_icon_css.svg', mobileSrc: "../../assets/icons/icon_project_css.svg", alt: 'Icon CSS' },
     { src: '../../assets/icons/skill_icon_bootstrap.png', mobileSrc: "../../assets/icons/icon_project_bootstrap.svg", alt: 'Icon Bootstrap' },
     { src: '../../assets/icons/skill_icon_md.svg', mobileSrc: "../../assets/icons/icon_project_md.svg", alt: 'Icon Material Design' },
-    { src: '../../assets/icons/skill_icon_vb.png', mobileSrc: "../../assets/icons/icon_project_vb.svg", alt: 'Icon VB' },
   ];
   backendSkills : { src: string; mobileSrc: string; alt: string }[] = [
-    { src: '../../assets/icons/icon_project_python.svg', mobileSrc:"", alt: 'Icon Python' },
-    { src: '../../assets/icons/icon_project_django.svg', mobileSrc:"", alt: 'Icon Django' },
-    { src: '../../assets/icons/icon_project_sql.svg', mobileSrc:"", alt: 'Icon SQL' },
-    { src: '../../assets/icons/icon_project_api.svg', mobileSrc: '../../assets/icons/skill_icon_api.svg', alt: 'Icon API' },
+    { src: '../../assets/icons/skill_icon_python.svg', mobileSrc:'../../assets/icons/icon_project_python.svg', alt: 'Icon Python' },
+    { src: '../../assets/icons/skill_icon_django.svg', mobileSrc:'../../assets/icons/icon_project_django.svg', alt: 'Icon Django' },
+    { src: '../../assets/icons/skill_icon_sql.svg', mobileSrc: '../../assets/icons/icon_project_sql.svg', alt: 'Icon SQL' },
+    { src: '../../assets/icons/skill_icon_api.svg', mobileSrc: '../../assets/icons/icon_project_api.svg', alt: 'Icon API' },
     { src: '../../assets/icons/skill_icon_firebase.svg', mobileSrc:"../../assets/icons/icon_project_firebase.svg", alt: 'Icon Firebase' },
-    { src: '../../assets/icons/icon_project_postman.svg', mobileSrc:"", alt: 'Icon Postman' },
-    { src: '../../assets/icons/icon_project_redis.svg', mobileSrc:"", alt: 'Icon Redis' },
-    { src: '../../assets/icons/icon_project_postgresql.svg', mobileSrc:"", alt: 'Icon PostgreSQL' },
-    { src: '../../assets/icons/icon_project_pytest.svg', mobileSrc:"", alt: 'Icon pytest' },
-    { src: '../../assets/icons/icon_project_docker.svg', mobileSrc:"", alt: 'Icon Docker' },
+    { src: '../../assets/icons/skill_icon_redis.svg', mobileSrc:'../../assets/icons/icon_project_redis.svg', alt: 'Icon Redis' },
+    { src: '../../assets/icons/skill_icon_postgresql.svg', mobileSrc:'../../assets/icons/icon_project_postgresql.svg', alt: 'Icon PostgreSQL' },
+    { src: '../../assets/icons/skill_icon_pytest.svg', mobileSrc:'../../assets/icons/icon_project_pytest.svg', alt: 'Icon pytest' },
+    { src: '../../assets/icons/skill_icon_docker.svg', mobileSrc:'../../assets/icons/icon_project_docker.svg', alt: 'Icon Docker' },
   ];
   toolsSkills : { src: string; mobileSrc:string; alt: string }[] = [
-    { src: '../../assets/icons/skill_icon_git.svg', mobileSrc:"", alt: 'Icon GIT' },
-    { src: '../../assets/icons/skill_icon_scrum.svg', mobileSrc:"", alt: 'Icon Scrum' },
+    { src: '../../assets/icons/skill_icon_vscode.svg', mobileSrc:"../../assets/icons/icon_project_vscode.svg", alt: 'Icon VSCode' },
+    { src: '../../assets/icons/skill_icon_github.svg', mobileSrc:'../../assets/icons/sm_button_github_hover_mint.svg', alt: 'Icon GIT' },
+    { src: '../../assets/icons/skill_icon_git.svg', mobileSrc:'../../assets/icons/icon_project_git.svg', alt: 'Icon GIT' },
+    { src: '../../assets/icons/skill_icon_scrum.svg', mobileSrc:'../../assets/icons/icon_project_scrum.svg', alt: 'Icon Scrum' },
+    { src: '../../assets/icons/skill_icon_linux.svg', mobileSrc:'../../assets/icons/icon_project_linux.svg', alt: 'Icon Linux' },
+    { src: '../../assets/icons/skill_icon_postman.svg', mobileSrc:'../../assets/icons/icon_project_postman.svg', alt: 'Icon Postman' },
   ];
 
   selectedSkillSet : { src: string; mobileSrc: string; alt: string }[] = this.frontendSkills;
